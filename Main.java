af02cf372539cac7cfe64205e001d1577cac4707
Vihanga <53087925+vihanga123@users.noreply.github.com>
2023-02-08 21:21:45 +0530

    HW Sensor information output
    AWT+ Oshi Sensor informations (broken)

--- Main.java
+++ Main.java
@@ -1,29 +1,59 @@
 package oshi;
 
-import oshi.hardware.CentralProcessor;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+import oshi.SystemInfo;
 import oshi.hardware.HardwareAbstractionLayer;
+import oshi.hardware.CentralProcessor;
+import oshi.hardware.Sensors;
+import oshi.driver.windows.LogicalProcessorInformation;
 
-import static javax.swing.JFrame.EXIT_ON_CLOSE;
-import javax.swing.*;
 
 
-public class SystemInfoTest {
+public class Main extends Frame {
+    Label LabelCPUtemp;    // Declare a Label component
+    TextField CPUTemp; // Declare a TextField component
+    Button refreshBtn;   // Declare a Button component
+    int count = 0;     // Counter's value
 
-    private JFrame mainFrame;
-    private JButton jmenu;
+    public Main () {
+        SystemInfo si =new SystemInfo();
+        HardwareAbstractionLayer hal = si.getHardware();
+        CentralProcessor cpu = hal.getProcessor();
+        Sensors sensors = hal.getSensors();
 
-    private SystemInfo si = new SystemInfo();
+        setLayout(new FlowLayout());
 
+        LabelCPUtemp = new Label("CPU Temp");
+        add(LabelCPUtemp);
+
+        CPUTemp = new TextField(hal.getSensors().toString()); //Arrays.toString(sensors.getFanSpeeds()
+        CPUTemp.setEditable(false);
+        add(CPUTemp);
+        refreshBtn = new Button("Refresh");
+        refreshBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Main.super.repaint();
+            }
+        });
+        add(refreshBtn);
+
+        setTitle("NetMeter");
+        setSize(300, 100);
+        setVisible(true);
+    }
+
+
+    // The entry main() method
     public static void main(String[] args) {
 
-        JFrame j= new JFrame();
-        JTextField txt1 = new JTextField();
-        add(java.awt.TrayIcon);
-        txt1.setBounds(120,150,120,40);
-        JButton b = new JButton("click");
-        b.setBounds(130,200,100,40);
-        j.add(b);
-        j.add(txt1);
-        j.setSize(400,500);
-        j.setLayout(null);
-        j.setVisible(true);
+        Main app = new Main();
+
+    }
+
+}
